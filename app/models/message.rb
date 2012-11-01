2172b290d1d4cf2315ae85d276e11e67066fb0c3
Hashrocket Workstation <dev@hashrocket.com>
2012-11-01 14:45:36 -0400

    Change back from OpenStruct

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,21 +1,19 @@
-require 'ostruct'
-
-class Message < Openstruct
+class Message
 
   include ActiveModel::Validations
   include ActiveModel::Conversion
   extend ActiveModel::Naming
 
-  # attr_accessor :name, :email, :subject, :body
+  attr_accessor :name, :email, :subject, :body
 
   validates :name, :email, :subject, :body, :presence => true
   validates :email, :format => { :with => %r{.+@.+\..+} }
 
-  # def initialize(attributes = {})
-  #   attributes.each do |name, value|
-  #     send("#{name}=", value)
-  #   end
-  # end
+  def initialize(attributes = {})
+    attributes.each do |name, value|
+      send("#{name}=", value)
+    end
+  end
 
   def persisted?
     false
