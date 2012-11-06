757dccfb0807ca1ed95c013d8dfb9e7be06674f0
Hashrocket Workstation <dev@hashrocket.com>
2012-11-06 14:16:21 -0500

    Add image move feature.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ActiveRecord::Base
-  attr_accessible :category, :title, :picture, :medium, :dimensions
+  attr_accessible :category, :title, :picture, :medium, :dimensions, :gallery_id
   belongs_to :gallery
   mount_uploader :picture, PictureUploader
 
