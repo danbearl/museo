81d36ca05254d0d4fe3c1be99a8689a04cdc20ae
Dan Bearl <dan.bearl@gmail.com>
2016-02-14 18:13:01 -0500

    Moving secrets to figaro

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider => 'AWS',
-    :aws_access_key_id => 'AKIAICOMXF6MSZG5XVUA',
-    :aws_secret_access_key => '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'
+    :aws_access_key_id => ENV["aws_access_key_id"]
+    :aws_secret_access_key => ENV["aws_secret_access_key"]
   }
   config.fog_directory = 'laurabearl'
   config.fog_public = false
